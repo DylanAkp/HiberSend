19bfee515bff158e7c8c91dd88412330539a9ef2
Dylan AKPINAR <dylan.akpinar@epitech.eu>
2022-01-12 23:47:05 +0100

    [ADDED] Linux Compatibility

---
+++ linux-install.sh
@@ -0,0 +1,4 @@
+npm install hibercli
+cp ./content/HiberSend ~/.local/share/nautilus/scripts/HiberSend
+chmod 777 ~/.local/share/nautilus/scripts/HiberSend
+echo "Installation done. Rightclick->Scripts->HiberSend"
