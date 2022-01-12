a04a24d6a14bdda7de44dd72e6a5d39f099ffaff
Dylan AKPINAR <dylan.akpinar@epitech.eu>
2022-01-13 00:12:22 +0100

    [ADDED] Error Handling

--- linux-install.sh
+++ linux-install.sh
@@ -1,3 +1,8 @@
+if ! which node > /dev/null
+then
+    echo "Node must be installed, script will be stopped"
+    exit
+fi
 npm install hibercli
 cp ./content/HiberSend ~/.local/share/nautilus/scripts/HiberSend
 chmod 777 ~/.local/share/nautilus/scripts/HiberSend
